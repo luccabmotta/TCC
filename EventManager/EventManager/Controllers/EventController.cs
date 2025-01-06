873aaaa4940fecfde2868873b45a97e61440a902
Lucca Motta <mottalucca@yahoo.com.br>
2025-01-06 11:48:20 -0300

    Initial commit

---
+++ EventManager/EventManager/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Evento 1";
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Evento {id}";
+        }
+    }
+}
